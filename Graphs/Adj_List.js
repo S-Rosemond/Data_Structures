71683412352220330886f28a161e93f348d26859
Samuel <js.rosemond@gmail.com>
2021-03-10 10:10:48 -0500

    Graphic basics done

--- Graphs/Adj_List.js
+++ Graphs/Adj_List.js
@@ -17,6 +17,27 @@ class Graph {
 
     return true;
   }
+  removeEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]?.includes(vertex2)) return false;
+    if (!this.adjacencyList[vertex2]?.includes(vertex1)) return false;
+
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (el) => el !== vertex2
+    );
+
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (el) => el !== vertex1
+    );
+  }
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) return false;
+
+    for (let el of this.adjacencyList[vertex]) {
+      this.removeEdge(el, vertex);
+    }
+
+    return !!delete this.adjacencyList[vertex];
+  }
 
   print() {
     console.log(this.adjacencyList);
@@ -32,5 +53,21 @@ console.log(myGraph.addVertex("Test"));
 
 myGraph.addEdge("Tokyo", "Aspen");
 console.log(myGraph.addEdge("Tokyo", "Aspen"));
-
+myGraph.addEdge("Test", "Aspen");
 console.log(myGraph.print());
+
+myGraph.removeEdge("Test", "Tokyo");
+myGraph.removeEdge("Test", "Aspen");
+console.log(myGraph.print());
+
+myGraph.addVertex("Spain");
+myGraph.addVertex("England");
+myGraph.addEdge("Tokyo", "Spain");
+myGraph.addEdge("Tokyo", "England");
+myGraph.addEdge("Aspen", "Spain");
+myGraph.addEdge("Aspen", "England");
+console.log(myGraph.print());
+
+const result = myGraph.removeVertex("Tokyo");
+
+console.log(myGraph.print(), result);
